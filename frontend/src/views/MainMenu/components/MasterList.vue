d9ae1542302543e59136c609c1462eb23409d1e4
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-12-29 12:44:38 +0900

    リンクの型だけ追加

--- frontend/src/views/MainMenu/components/MasterList.vue
+++ frontend/src/views/MainMenu/components/MasterList.vue
@@ -1,8 +1,16 @@
 <template>
-    <div>
-        <p>要素3</p>
-        <p>要素4</p>
-    </div>
+    <div style='text-align:center;font-size:12px;'>
+        <router-link
+            :to="{ name: '' }">番組登録</router-link></div>
+    <div style='text-align:center;font-size:12px;'>
+        <router-link
+            :to="{ name: '' }">タレント登録</router-link></div>
+    <div style='text-align:center;font-size:12px;'>
+        <router-link
+            :to="{ name: '' }">年月週管理登録</router-link></div>
+    <div style='text-align:center;font-size:12px;'>
+        <router-link
+            :to="{ name: '' }">オンエア管理登録</router-link></div>
 </template>
 <script>
 export default {
